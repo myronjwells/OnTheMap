b584720926b5bc5fbae77994919fcb516f8ea00c
mwells <mwells@haystax.com>
2020-02-21 12:26:33 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Myron Wells on 2/21/20.
+//  Copyright © 2020 Myron Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
